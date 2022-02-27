6b55469d68bc03b39cfac6bd1716148919bae0aa
Ilie Cristian Dorobat <idorobat@cisco.com>
2022-02-28 01:22:35 +0200

    Add public constants to Thread

--- src/ro/webdata/echo/commons/Thread.java
+++ src/ro/webdata/echo/commons/Thread.java
@@ -1,6 +1,11 @@
 package ro.webdata.echo.commons;
 
 public class Thread {
+    /** The maximum number of times the application will attempt to get data if an error is encountered */
+    public static final int ERROR_THRESHOLD = 5;
+    public static final int DEFAULT_SLEEP_TIME = 100;
+    public static final int ERROR_SLEEP_TIME = 5000;
+
     public static void sleep(int counter, int divisor) {
         if (counter > 0 && counter % divisor == 0) {
             sleep(0);
